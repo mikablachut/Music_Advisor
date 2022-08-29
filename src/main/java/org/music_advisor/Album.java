175752e566ac6293c3de9907eaa6fe2055b106c5
Mika <mikari00@gamil.com>
2022-08-29 10:43:19 +0200

    Added: - new class Album, - new interface Request, - new method getJsonObjectAsString.

---
+++ src/main/java/org/music_advisor/Album.java
@@ -0,0 +1,48 @@
+package org.music_advisor;
+
+import java.util.Arrays;
+
+public class Album {
+    private String albumTitle;
+    private String[] listOfArtist;
+    private String link;
+
+    public Album() {
+
+    }
+
+    public Album(String albumTitle, String[] listOfArtist, String link) {
+        this.albumTitle = albumTitle;
+        this.listOfArtist = listOfArtist;
+        this.link = link;
+    }
+
+    public String getAlbumTitle() {
+        return albumTitle;
+    }
+
+    public void setAlbumTitle(String albumTitle) {
+        this.albumTitle = albumTitle;
+    }
+
+    public String[] getListOfArtist() {
+        return listOfArtist;
+    }
+
+    public void setListOfArtist(String[] listOfArtist) {
+        this.listOfArtist = listOfArtist;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        return albumTitle + "\n" + Arrays.toString(listOfArtist) + "\n" + link + "\n";
+    }
+}
--- src/main/java/org/music_advisor/NewReleases.java
+++ src/main/java/org/music_advisor/NewReleases.java
@@ -1,24 +1,52 @@
 package org.music_advisor;
 
-import java.net.URI;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NewReleases {
+public class NewReleases implements Request {
     private static final String NEW_RELEASES = "/v1/browse/new-releases";
 
-    public void getNewReleases() {
+    public static void getNewReleases() {
         HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest request = Request.getRequest(Main.API_SERVER_PATH + NEW_RELEASES);
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .headers("Content-Type", "application/json", "Authorization", "Bearer " + Authorization.ACCESS_TOKEN)
-                .uri(URI.create(Main.API_SERVER_PATH + NEW_RELEASES))
-                .GET()
-                .build();
-
-
-
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            getJsonObjectAsString(response);
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error response");
+        }
     }
 
+    private static void getJsonObjectAsString(HttpResponse<String> response) {
+        List<Album> listOfAlbums = new ArrayList<>();
+        String responseInJson = response.body();
+        JsonObject jsonObject = JsonParser.parseString(responseInJson).getAsJsonObject();
+        JsonObject albums = jsonObject.getAsJsonObject("albums");
 
+        for (JsonElement item : albums.getAsJsonArray("items")) {
+            Album album = new Album();
+            album.setAlbumTitle(item.getAsJsonObject().get("name").toString().replace("\"", ""));
+
+            for (JsonElement artist : item.getAsJsonObject().getAsJsonArray("artists")) {
+                String[] arrayOfArtist;
+                arrayOfArtist = new String[]{artist.getAsJsonObject().get("name").toString()
+                        .replace("\"", "")};
+                album.setListOfArtist(arrayOfArtist);
+            }
+            album.setLink(item.getAsJsonObject().get("external_urls").getAsJsonObject().get("spotify").toString()
+                          .replace("\"",""));
+            listOfAlbums.add(album);
+        }
+        for (Album album : listOfAlbums) {
+            System.out.println(album);
+        }
+    }
 }
