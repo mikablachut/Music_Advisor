8d2fb1ed4b2f3deba5e84a31b9e084744bf04881
Mika <mikari00@gamil.com>
2022-08-31 11:54:58 +0200

    Working Stage4. One class was added PlaylistCategory. The name of interface was changed from Request RequestAndResponse.

--- src/main/java/org/music_advisor/FeaturedPlaylists.java
+++ src/main/java/org/music_advisor/FeaturedPlaylists.java
@@ -3,35 +3,25 @@ package org.music_advisor;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FeaturedPlaylists implements Request {
+public class FeaturedPlaylists implements RequestAndResponse {
 
     private static final String FEATURED_PLAYLISTS = "/v1/browse/featured-playlists";
     private static final FeaturedPlaylists featuredPlaylists = new FeaturedPlaylists();
 
     public static void getFeaturedPlaylists() {
-        HttpClient httpClient = HttpClient.newBuilder().build();
-        HttpRequest request = Request.getRequest(Main.API_SERVER_PATH + FEATURED_PLAYLISTS);
 
-        try {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            featuredPlaylists.getJsonObjectAsString(response);
-        } catch (IOException | InterruptedException e) {
-            System.out.println("Error response");
-        }
+        String response = RequestAndResponse.getRequest(Main.API_SERVER_PATH + FEATURED_PLAYLISTS);
+        featuredPlaylists.getJsonObjectAsString(response);
+
     }
 
     @Override
-    public void getJsonObjectAsString(HttpResponse<String> response) {
+    public void getJsonObjectAsString(String response) {
         List<Playlist> listOfPlaylists = new ArrayList<>();
-        String responseInJson = response.body();
-        JsonObject jsonObject = JsonParser.parseString(responseInJson).getAsJsonObject();
+        JsonObject jsonObject = JsonParser.parseString(response).getAsJsonObject();
         JsonObject playlists = jsonObject.getAsJsonObject("playlists");
 
         for (JsonElement item : playlists.getAsJsonArray("items")) {
